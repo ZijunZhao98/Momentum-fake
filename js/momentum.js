bef638f4541d17df58481c3e71599427e2321ce3
zijunzhao <z.zhao@wustl.edu>
2018-08-10 15:26:30 -0500

    time working

--- js/momentum.js
+++ js/momentum.js
@@ -1,13 +1,20 @@
 function getT(){
-  console.log("inside get t");
   var now = new Date();
   var hour = now.getHours();
   var minute = now.getMinutes();
-  document.getElementById("time").innerHtml = hour + ":" + minute;
+  if(hour < 12 && hour > 4){
+     msg =  "Good morning.";
+  }else if(hour < 18){
+     msg = "Good afternoon.";
+  }else{
+     msg = "Good evening."
+  }
+
+  document.getElementById("time").innerHTML = hour + ":" + minute;
+  document.getElementById("salutation").innerHTML = msg;
 };
 
 window.onload = function(){
-  console.log("inside here");
+  getT();
   setInterval(getT(), 30000);
-  console.log("I am up");
 };
